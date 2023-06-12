5608f03bb7393e47a7e9aa5ace00df3cf5e5c73e
oppalaa <oppala7@gmail.com>
2023-06-12 18:32:14 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def nvn(request):
+    return render (request,'nvn.html')
+def nvn1(request):
+    return render (request,'nvn1.html')
